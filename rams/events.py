4e2ee0e58143c873dd116cc7c6c631f685149e25
OrochimaruBotz <109642842+OrochimaruBotz@users.noreply.github.com>
2022-08-21 17:30:42 +0700

    Update events.py

--- rams/events.py
+++ rams/events.py
@@ -194,7 +194,7 @@ def register(**args):
                 if not disable_errors:
                     date = strftime("%Y-%m-%d %H:%M:%S", gmtime())
 
-                    text = "**✨ RAM-UBOT LAPORAN EROR  ✨**\n\n"
+                    text = "**✨ GOJO-UBOT LAPORAN EROR  ✨**\n\n"
                     link = "[Group Support](https://t.me/ramsupportt)"
                     text += "Jika mau, Anda bisa melaporkan error ini, "
                     text += f"Cukup forward saja pesan ini ke {link}.\n\n"
